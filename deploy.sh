42a7f7d0e5de5e080310c74b9802815fb2bc9fbb
xff1024 <42406490+xff1024@users.noreply.github.com>
2021-04-24 12:36:48 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 set -e
 
 # 生成静态文件
+npm install
 npm run build
 
 # 进入生成的文件夹
@@ -13,5 +14,5 @@ git init
 git add -A
 git commit -m 'deploy'
 #https://github.com/xff1024/xff1024.github.io.git
-git push -f git@github.com:xff1024/xff1024.github.io.git master
+git push -f git@github.com:xff1024/xff1024.github.io.git master:gh-pages
 cd -
